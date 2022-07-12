781a145ffd883de02ecd04431ca7e05473196cc2
Clara <107092024+cocaclara@users.noreply.github.com>
2022-07-12 16:04:04 +0900

    second commit

--- js/basic.js
+++ js/basic.js
@@ -14,7 +14,16 @@ $(document).ready(function(){
   var lastScrollTop = 0,
       delta = 15;
   $(window).scroll(function(){
-    
+    var currentTop = $(this).scrollTop();
+    if( Math.abs(lastScrollTop - currentTop) <= delta )
+    return; // 스크롤값을 받아서 리턴한다.
+
+    if( currentTop > lastScrollTop && lastScrollTop > 0 ){ // 스크롤을 내렸을때 
+      $('.header-wrap').slideUp();
+    }else{  // 스크롤을 올렸을때
+      $('.header-wrap').slideDown();
+    }
+    lastScrollTop = currentTop;
   });
 });
 
