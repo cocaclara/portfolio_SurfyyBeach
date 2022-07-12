7bec5b77ad45e74b0a4552d7ac651f55611f8fb5
Clara <107092024+cocaclara@users.noreply.github.com>
2022-07-13 00:15:15 +0900

    ms commit

--- js/basic.js
+++ js/basic.js
@@ -10,9 +10,11 @@ $(document).ready(function(){
     $('.header-wrap').removeClass('on');
     $(this).find('.lnb').stop().slideUp(200);
   });
+  
   // 스크롤 내릴 시, 헤더 사라지고 스크롤 올릴 시 생김
   var lastScrollTop = 0,
       delta = 15;
+      
   $(window).scroll(function(){
     var currentTop = $(this).scrollTop();
     if( Math.abs(lastScrollTop - currentTop) <= delta )
@@ -24,6 +26,18 @@ $(document).ready(function(){
       $('.header-wrap').slideDown();
     }
     lastScrollTop = currentTop;
+
+    // 섹션1 지날 때, 탑버튼 생성
+    if( currentTop >= 300 ){
+      $('.top').fadeIn();
+    }else{
+      $('.top').fadeOut();
+    }
+  });
+  // 탑버튼 클릭 시, 최상단으로 이동
+  $('.top').click(function(){
+    $('html,body').animate({scrollTop:0},500);
+    return false;
   });
 });
 
