2ac597ff3861916d20c0b415f06c6704042f044f
Clara <107092024+cocaclara@users.noreply.github.com>
2022-07-12 15:24:27 +0900

    second commit

--- js/basic.js
+++ js/basic.js
@@ -2,22 +2,19 @@
 
 $(document).ready(function(){
   // 네비게이션 제어
-  $('.header-wrap').mouseover(function(){
-    $('.header-wrap').addClass('on');
-  });
-  $('.header-wrap').mouseout(function(){
-    $('.header-wrap').removeClass('on');
-    $(this).find('.lnb').css('display','none');
-  });
   $('.gnb li').mouseover(function(){
-    // $(this).find('.lnb').stop().slideDown(300);
+    $('.header-wrap').addClass('on');
+    $(this).find('.lnb').stop().slideDown();
   });
   $('.gnb').mouseout(function(){
     $('.header-wrap').removeClass('on');
-    // $(this).find('.lnb').stop().slideUp(300);
-
+    $(this).find('.lnb').stop().slideUp(200);
   });
-
-
+  // 스크롤 내릴 시, 헤더 사라지고 스크롤 올릴 시 생김
+  var lastScrollTop = 0,
+      delta = 15;
+  $(window).scroll(function(){
+    
+  });
 });
 
