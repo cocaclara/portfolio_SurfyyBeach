ec57ab82df6bb7283f06f29d1e867243f9e770e2
penta-di-mono <madamea521@gmail.com>
2022-07-12 16:03:57 +0900

    hyeonmi first commit

--- js/basic.js
+++ js/basic.js
@@ -2,25 +2,18 @@
 
 $(document).ready(function(){
   // 네비게이션 제어
-  $('.header-wrap').mouseover(function(){
+  $('.gnb li').mouseover(function(){
     $('.header-wrap').addClass('on');
-  });
-  $('.header-wrap').mouseout(function(){
-    $('.header-wrap').removeClass('on');
-    $(this).find('.lnb').css('display','none');
-  });
-  $('.gnb>li').mouseover(function(){
-    if($('.gnb>li').children('ul.lnb')){
-      $(this).find('.lnb').stop().slideDown(300);
-    }
-    // $(this).find('.lnb').stop().slideDown(300);
+    $(this).find('.lnb').stop().slideDown();
   });
   $('.gnb').mouseout(function(){
     $('.header-wrap').removeClass('on');
-    // $(this).find('.lnb').stop().slideUp(300);
-
+    $(this).find('.lnb').stop().slideUp(200);
   });
-
-
+  // 스크롤 내릴 시, 헤더 사라지고 스크롤 올릴 시 생김
+  var lastScrollTop = 0,
+      delta = 15;
+  $(window).scroll(function(){
+    
+  });
 });
-
